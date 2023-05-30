ba68c59c9bc174548ed00af0725f7860a2360c99
aisha <aicha.ouadah.usthb@gmail.com>
2023-05-30 17:39:30 +0100

    solution challenge2

--- level2/second_challenge_solution.py
+++ level2/second_challenge_solution.py
@@ -1,4 +1,11 @@
-def solution(L):
+from itertools import permutations
+def solution2(L):
+    L.sort(reverse=True)
+    for length in range(len(L), 0, -1):
+        for perm in permutations(L, length):
+            num = int(''.join(map(str, perm)))
+            if num % 3 == 0:
+                return num
     return 0
-
-solution([3,1,4,1])
+print(solution2([3,1,4,1]))
+print(solution2([3, 1, 4, 1, 5, 9])) 
